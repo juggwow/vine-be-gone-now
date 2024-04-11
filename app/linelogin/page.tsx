49f08ce6a94d2b56d4721094cb7ab214d6472bdd
patna <p.wiriyar@gmail.com>
2024-04-11 17:05:38 +0700

    remove auth option

--- app/linelogin/page.tsx
+++ app/linelogin/page.tsx
@@ -1,14 +1,15 @@
 import { getServerSession } from "next-auth";
 import { signIn } from "next-auth/react";
 import { useEffect } from "react";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import Login from "./client";
+import { authOptions } from "../api/auth/[...nextauth]/auth-options";
 
 export default async function LineLogin() {
   const session = await getServerSession(authOptions);
   if (session) {
     return redirect("/");
   }
+  
   return <Login />;
 }
