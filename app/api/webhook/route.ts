7a985ecebd061658b4aea2c4986ca881715d7e9b
patna <p.wiriyar@gmail.com>
2024-04-28 13:51:03 +0700

    fixed webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,33 +3,33 @@
 import clientPromise from "@/lib/mongodb";
 
 export async function POST(req: Request) {
-    const mongoClient = await clientPromise
-    mongoClient.connect()
-    mongoClient.db("vine-be-gone").collection("webhook").insertOne({
-        req: await req.json()
-    })
-//   const reqObj = (await req.json())[0];
-//   console.log(reqObj)
-//   const message = reqObj.message.text as string;
-//   const replyToken = reqObj.replyToken;
-//   const resLineApi = await fetch("https://api.line.me/v2/bot/message/reply", {
-//     method: "POST",
-//     headers: {
-//       Authorization: `Bearer ${process.env.NEXT_PUBLIC_MESSAGING_TOKEN}`,
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       replyToken,
-//       messages: [
-//         {
-//           type: "text",
-//           text: `ตอบกลับ ${message}`,
-//         },
-//       ],
-//     }),
-//   });
+    // const mongoClient = await clientPromise
+    // mongoClient.connect()
+    // mongoClient.db("vine-be-gone").collection("webhook").insertOne({
+    //     req: await req.json()
+    // })
+  const reqObj = (await req.json()).events[0];
+  console.log(reqObj)
+  const message = reqObj.message.text as string;
+  const replyToken = reqObj.replyToken;
+  const resLineApi = await fetch("https://api.line.me/v2/bot/message/reply", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_MESSAGING_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      replyToken,
+      messages: [
+        {
+          type: "text",
+          text: `ตอบกลับ ${message}`,
+        },
+      ],
+    }),
+  });
 
   return new Response("", {
-    status: 200,
+    status: resLineApi.status,
   });
 }
